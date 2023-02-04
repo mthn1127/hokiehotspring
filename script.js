68318ab389d58ec46105cfbeab1fd4ed81364cd6
mthn1127 <minhthu@vt.edu>
2023-02-04 17:40:48 -0500

    links work finally?

---
+++ script.js
@@ -0,0 +1,9 @@
+function help1()
+{
+    alert("General Contacts:\n\u2022For questions about applying for admissions or checking the "
+        + "status of your application, contact the Admissions Office.\n\u2022For questions about adding "
+        + "or dropping classes, checking seat availability in a class, getting an unofficial transcript, "
+        + "seeing your grades, or reviewing account charges and balance information, contact the Registrar's Office."
+        + "\n\u2022For questions about payroll, contact the Payroll Office.\n\u2022For questions about alumni "
+        + "activities, contact the Alumni Office."
+}
